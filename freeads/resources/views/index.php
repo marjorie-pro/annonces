f9765552603bc25af230571c020f2064d74b54a9
marjo <pezeron.marjorie@gmail.com>
2019-10-01 16:27:26 +0200

    envoi debut freeads

---
+++ freeads/resources/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Freeads</title>
+</head>
+<body>
+	<h1>Hello</h1>
+	<p>Vous êtes sur la page d'accueil</p>
+
+</body>
+</html>
